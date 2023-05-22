0161cbd411e39b581844e9fbb5a5521761ac8755
Nitika Chandan <134251464+neeteekaa@users.noreply.github.com>
2023-05-22 00:53:41 -0400

    JS file added

---
+++ index.js
@@ -0,0 +1,35 @@
+//Task 1 starts here//
+
+var a = 3;
+var b = 5;
+var c;
+var result = "";
+result += 'var a = 3;\n';
+result += 'var b = 5;\n';
+result += 'var c;\n';
+result += '----------\n';
+result += 'a + b = ' + (a + b) + '\n';
+result += 'a - b = ' + (a - b) + '\n';
+result += 'a * b = ' + (a * b) + '\n';
+result += 'a / b = ' + (a / b) + '\n';
+result += 'a % b = ' + (a % b) + '\n';
+result += 'a += b = ' + (a += b) + '\n';
+result += 'a -= b = ' + (a -= b) + '\n';
+result += 'a *= b = ' + (a *= b) + '\n';
+result += 'a /= b = ' + (a /= b) + '\n';
+result += 'a %= b = ' + (a %= b) + '\n';
+result += 'a == b = ' + (a == b) + '\n';
+result += 'a != b = ' + (a != b) + '\n';
+result += 'a > b = ' + (a > b) + '\n';
+result += 'a < b = ' + (a < b) + '\n';
+result += '!a && !c = ' + (!a && !c) + '\n';
+result += '!a || !c = ' + (!a || !c) + '\n';
+alert(result);
+
+//Task 2 starts here//
+
+var first_name = "Nitika";
+var last_name = "Chandan";
+var email = "chan0541@algonquinlive.com";
+var output = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
+alert(output);
