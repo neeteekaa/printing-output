6fd2814f1503df72ba4952a998f59f4fe5c3e09c
Nitika Chandan <134251464+neeteekaa@users.noreply.github.com>
2023-05-22 00:05:43 -0400

    JS file added

---
+++ index.js
@@ -0,0 +1,5 @@
+var first_name = "Nitika";
+var last_name = "Chandan";
+var email = "chan0541@algonquinlive.com";
+var output = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
+alert(output);
