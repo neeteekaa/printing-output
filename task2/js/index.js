be65c955da032ff99e3d735500d939b8f60ee6cb
Nitika Chandan <134251464+neeteekaa@users.noreply.github.com>
2023-05-22 00:49:17 -0400

    Delete task2 directory

--- task2/js/index.js
+++
@@ -1,5 +0,0 @@
-var first_name = "Nitika";
-var last_name = "Chandan";
-var email = "chan0541@algonquinlive.com";
-var output = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
-alert(output);
