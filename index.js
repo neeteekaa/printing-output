fe9b8966a1ba1899320e88d8760059baa1d95b65
Nitika Chandan <134251464+neeteekaa@users.noreply.github.com>
2023-05-22 22:33:41 -0400

    JS file added

---
+++ index.js
@@ -0,0 +1,37 @@
+//task 1 starts here//
+
+let a = 3;
+let b = 5;
+let c;
+let result = `
+let a = 3;
+let b = 5;
+let c;
+----------
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}
+`;
+alert(result);
+
+
+//task 2 starts here//
+
+let first_name = "Nitika";
+let last_name = "Chandan";
+let email = "chan0541@algonquinlive.com";
+let output = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
+alert(output);
